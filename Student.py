d79b3c6e0a241145004c2f7f574ab44d8de0d9fc
ritraj5 <ritu.raj@cj.com>
2022-04-19 12:49:39 +0530

    Updated

--- Student.py
+++ Student.py
@@ -3,10 +3,32 @@ class Student:
         self.sid = sid
         self.name = name
         self.m = m
+
     def calResult(self,m):
         total = sum(m)
         percentage = total/4
         return total,percentage
+
+    def calGrade(self,p):
+        (total,p1) = p
+        print(p1)
+        if p1 >= 90:
+            print('Distinction')
+            return 'Distinction'
+        elif p1 < 90 and p1>=60:
+            print('First Class')
+            return 'First Class'
+        elif p1 < 60 and p1>=50:
+            print('Second Class')
+            return 'Second Class'
+        elif p1< 50 and p1>=40:
+            print('Pass')
+            return 'Pass'
+        else:
+            print('Fail')
+            return 'Fail'
+
     
 s1 = Student(1,'RR',[23,45,67,1])
-print(s1.calResult([23,45,67,1]))
+#print(s1.calResult([23,45,67,1]))
+s1.calGrade(s1.calResult([23,45,67,100]))
--- Student_test.py
+++ Student_test.py
@@ -6,5 +6,11 @@ class Student_Test_case(unittest.TestCase):
   def test_student_test_1(self):
     s = Student(1,'RR',[34,56,78,39])
     result=s.calResult([34,56,78,39])
-    print(result)
+    #print(result)
     self.assertEqual(result,(207,51.75))
+
+  def test_student_test_2(self):
+    s = Student(1,'RR',[34,56,78,39])
+    result=s.calGrade(s.calResult([34,56,78,39]))
+    #print(result)
+    self.assertEqual(result,'Second Class')
